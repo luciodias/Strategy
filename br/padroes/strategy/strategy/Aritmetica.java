1f5f5e1127e914c88b2acbd652baabcad60faf80
Lucio Dias <luciodiass@gmail.com>
2025-10-22 08:27:04 -0300

    classes iniciais

---
+++ br/padroes/strategy/strategy/Aritmetica.java
@@ -0,0 +1,15 @@
+package br.padroes.strategy.strategy;
+
+public class Aritmetica implements MediaStrategy {
+
+    @Override
+    public double calcularMedia(double p1, double p2) {
+        return (p1 + p2) / 2;
+    }
+
+    @Override
+    public String verificarSituacao(double media) {
+        return media >= 6.0 ? "Aprovado" : "Reprovado";
+    }
+    
+}
---
+++ br/padroes/strategy/strategy/Geometrica.java
@@ -0,0 +1,15 @@
+package br.padroes.strategy.strategy;
+
+public class Geometrica implements MediaStrategy {
+
+    @Override
+    public double calcularMedia(double p1, double p2) {
+        return Math.sqrt(p1 * p2);
+    }
+
+    @Override
+    public String verificarSituacao(double media) {
+        return media >= 6.0 ? "Aprovado" : "Reprovado";
+    }
+    
+}
